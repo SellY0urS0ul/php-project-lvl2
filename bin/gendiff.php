dc1b25c1d74627104cfa954c48baaccdd845f8d4
SellY0urSoul <valentintihonov9@gmail.com>
2021-12-13 15:13:04 +0300

    Create bin/gendiff.php. First step done.

---
+++ bin/gendiff.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+$doc = <<<DOC
+
+Generate diff
+
+Usage:
+  ./bin/gendiff.php (-h | --help)
+  ./bin/gendiff.php (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version     Show version.
+DOC;
+
+$args = Docopt::handle($doc, array('version'=>'Gendiff 1.0'));
+foreach ($args as $k=>$v)
+    echo $k.': '.json_encode($v).PHP_EOL;
