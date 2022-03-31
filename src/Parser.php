7c8c0661abf160564c337366bb99be95ae82857c
SellY0urS0ul <valentintihonov9@gmail.com>
2022-03-31 20:47:54 +0300

    test

--- src/Parser.php
+++ src/Parser.php
@@ -9,15 +9,15 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse(string $path): array
 {
-    if (file_exists($path)) {
-        if (substr($path, -4) === "json") {
-            $fileContent = json_decode(file_get_contents($path), true);
-            return $fileContent;
-        } else {
-            $fileContent = Yaml::parseFile($path);
-            return $fileContent;
-        }
+    if (!file_exists($path)) {
+        throw new Exception("Invalid file path: {$path}");
+    }
+
+    if (substr($path, -4) === "json") {
+        $fileContent = json_decode(file_get_contents($path), true);
+        return $fileContent;
     } else {
-        throw new Exception('Нет такого файла!');
+        $fileContent = Yaml::parseFile($path);
+        return $fileContent;
     }
 }
