342eaba580b106c0256842cc9cac50a8522cce8c
SellY0urS0ul <valentintihonov9@gmail.com>
2022-03-31 20:42:24 +0300

    test

--- src/Parser.php
+++ src/Parser.php
@@ -2,17 +2,22 @@
 
 namespace Php\Project\Lvl2\Parser;
 
+use Exception;
 use Symfony\Component\Yaml\Yaml;
 
 // Функция, принимающая на вход путь к файлу и возвращающая массив с его содержимым
 
 function parse(string $path): array
 {
-    if (substr($path, -4) === "json") {
-        $fileContent = json_decode(file_get_contents($path), true);
-        return $fileContent;
+    if (file_exists($path)) {
+        if (substr($path, -4) === "json") {
+            $fileContent = json_decode(file_get_contents($path), true);
+            return $fileContent;
+        } else {
+            $fileContent = Yaml::parseFile($path);
+            return $fileContent;
+        }
     } else {
-        $fileContent = Yaml::parseFile($path);
-        return $fileContent;
+        throw new Exception('Нет такого файла!');
     }
 }
